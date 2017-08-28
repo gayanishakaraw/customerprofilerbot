2799d255b7c51c8d4a6344dd4ac9b1693042917d
Gayan Wimalarathna <gwimalarathna@Virtusa.com>
2017-08-28 17:51:04 +0530

    Added items carosol

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
   Documentation: https://botpress.io/docs
   Our Slack Community: https://slack.botpress.io
 */
-const staticData = require('../static_data')
+
 
 module.exports = function(bp) {
     //var username = profile.id;
@@ -29,6 +29,16 @@ module.exports = function(bp) {
     var size = undefined;
     var sleeveType = undefined;
     var color = undefined;
+
+    //const staticData = require('../static_data');
+
+    var catalog = [
+    { name: 'LeBond Fashion - Slim Fit Shirt', sku: 'LBFS-81060-COL04', image: 'https://hameedia.com/media/catalog/product/optimized/0/4/04f2fc83c449f2edc679b5ecfa45d7b0/aus_web-445_0001_layer_18.jpg', price: 'Rs 1,990.00' },
+    { name: 'Envoy London Tailored Fit - Platinum Collection', sku: 'ENPC-5138522-COL04', image: 'https://hameedia.com/media/catalog/product/optimized/2/c/2cf3b70370d0fce4aebe111bc2e5ca43/28_1.jpg', price: 'Rs 4,495.00' },
+    { name: 'Envoy London - Ceremonial Shirt', sku: 'ECLR-5130865-White', image: 'https://hameedia.com/media/catalog/product/optimized/5/f/5fe08e7a485d8b894ca21b7be461c715/37.jpg', price: 'Rs 5,995.00' },
+    { name: 'Envoy Sport Regular Fit Shirt', sku: 'ENSP-80091-2', image: 'https://hameedia.com/media/catalog/product/optimized/0/d/0d503f086f31d334f16152e65b912931/hho_7093_copy.jpg', price:'Rs 2,995.00' },
+    { name: 'LB Sport Slim Fit Shirt', sku: 'LBSP-79690-2-Blue', image: 'https://hameedia.com/media/catalog/product/optimized/0/d/0d503f086f31d334f16152e65b912931/hho_7122_copy_1.jpg', price:'Rs 1,995.00' }
+  ];
 
   // Listens for a first message (this is a Regex)
   // GET_STARTED is the first message you get on Facebook Messenger
@@ -114,7 +124,7 @@ bp.hear('ITEMSSELECTION_REPLY.B1', (event, next) => {
 })
 
 bp.hear(/QR_(OVEL|TRANGLE|RECTANGLE|RHOMBOID|ITRANGLE)_BUTTON/, (event, next) => {
-  event.reply('#bodyTypeSection_reply', { bodyType: event.captured[0].toLowerCase() })
+  event.reply('#bodyTypeSection_reply', { bodyType: event.captured[0].toLowerCase() }, { items:this.catalog })
 })
 
 //bp.hear('GENDERSELECTION_REPLY', (event, next) => {
